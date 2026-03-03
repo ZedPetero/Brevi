27d6712703e2313fc62f146f1c3089041d703fd1
jbalbero.553703 <j.balbero.553703@umindanao.edu.ph>
2026-03-03 08:19:32 +0800

    New Branch made

--- Main_Screen/Properties/Resources.Designer.cs
+++ Main_Screen/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace AE.Application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap editbtn {
+            get {
+                object obj = ResourceManager.GetObject("editbtn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
